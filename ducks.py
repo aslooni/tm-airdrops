9807e28441d9600eb1fbbaabb639125e11e12001
moslem <alone.m2a@gmail.com>
2024-10-01 20:27:48 +0330

    1

--- ducks.py
+++ ducks.py
@@ -3,12 +3,12 @@ import json
 
 url = "https://api.apiduck.xyz/user-partner-mission/claim"  
 headers = {  
-    "authorization": "Bearer $Token",  
+    "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjUxNTQ5NzEsInRpbWVzdGFtcCI6MTcyNzU1NzU2NTk4OCwidHlwZSI6MSwiaWF0IjoxNzI3NTU3NTY1LCJleHAiOjE3MjgxNjIzNjV9.W8f8vAFd3Gev4ln8pP8nz3FG7-zamaFHYT0HCMvbe3Y",  
     "content-type": "application/json",  
 }  
 
-# 681
-for i in range(1, 681):  
+# 727
+for i in range(727, 800):  
     data = {"partner_mission_id": i}  
     response = requests.post(url, headers=headers, data=json.dumps(data))  
     print(f"Request {i}: {response.text}")
